bdd1a50ea7e6be4df524f0fd0da457970d914e9c
leokoy <111@163.com>
2019-08-13 17:51:09 +0800

    增加提交

--- tensquare_base/src/main/java/com/itheima/controller/LabelController.java
+++ tensquare_base/src/main/java/com/itheima/controller/LabelController.java
@@ -2,11 +2,13 @@ package com.itheima.controller;
 
 import com.itheima.pojo.Label;
 import com.itheima.service.LabelService;
+import entity.PageResult;
 import entity.Result;
 import entity.StatusCode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
-
+import java.util.Map;
 import java.util.List;
 
 /**
@@ -66,6 +68,29 @@ public class LabelController {
     public Result findAll(){
         List<Label> labelList = labelService.findAll();
         return new Result(true, StatusCode.OK,"查询所有标签成功",labelList);
+    }
+
+    /**
+     *
+     *条件查询标签信息
+     *以后注意：可以使用Map更为灵活，当前使用Label没有问题
+     *页面：开始时间  结束时间
+     *实体类中只有一个 创建时间
+     */
+    @RequestMapping(value = "/search",method = RequestMethod.POST)
+    public Result search(@RequestBody Map<String,String> map){
+        List<Label> labelList = labelService.search(map);
+        return new Result(true, StatusCode.OK,"条件查询标签信息成功",labelList);
+    }
+
+    /**
+     *
+     条件查询标签分页
+     */
+    @RequestMapping(value = "/search/{page}/{size}",method = RequestMethod.POST)
+    public Result findPage(@RequestBody Map<String,String> map,@PathVariable int page,@PathVariable int size){
+        Page<Label> labelPage = labelService.findPage(map,page,size);
+        return new Result(true, StatusCode.OK,"条件查询标签分页成功",new PageResult<>(labelPage.getTotalElements(),labelPage.getContent()));
     }
 
 }
--- tensquare_base/src/main/java/com/itheima/service/LabelService.java
+++ tensquare_base/src/main/java/com/itheima/service/LabelService.java
@@ -3,11 +3,23 @@ package com.itheima.service;
 import com.itheima.dao.LabelDao;
 import com.itheima.pojo.Label;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 import util.IdWorker;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 只有类，没有接口，业务比较简单，
@@ -56,5 +68,96 @@ public class LabelService {
      */
     public List<Label> findAll() {
         return labelDao.findAll();
+    }
+
+    /**
+     * 条件查询标签信息
+     * @param map
+     * @return
+     */
+    public List<Label> search(Map<String, String> map) {
+        //通过匿名内部类拼接查询条件
+        /*Specification<Label> spec = new Specification<Label>() {
+            *//**
+             * @param root 操作根对象
+             * @param criteriaQuery 了解 顶级查询对象
+             * @param criteriaBuilder 条件构建对象
+             * @return
+             *//*
+            @Nullable
+            @Override
+            public Predicate toPredicate(Root<Label> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
+                // "labelname": "string",  "state": "string",
+
+                String labelname = map.get("labelname");//标签名称  模糊查询
+                String state = map.get("state");//精确匹配
+                //定一个list存放条件
+                List<Predicate> list = new ArrayList<>();
+                if(!StringUtils.isEmpty(labelname)){
+                    list.add(criteriaBuilder.like(root.get("labelname").as(String.class),"%"+labelname+"%")); // labelname like '%%'
+
+                }
+                if(!StringUtils.isEmpty(state)){
+                    list.add(criteriaBuilder.equal(root.get("state").as(String.class),state)); // labelname ='xxx'
+                }
+                if(list == null || list.size() == 0){
+                    return null;//没有条件
+                }
+                //将list转predicate数组
+               *//* Predicate[] predicates = new Predicate[list.size()];
+                Predicate[] newPredicates = list.toArray(predicates);
+                return criteriaBuilder.and(newPredicates);*//*
+               return criteriaBuilder.and(list.toArray(new Predicate[list.size()]));
+            }
+        };*/
+        return labelDao.findAll(getSpecification(map));
+    }
+    /**
+     *
+     条件查询标签分页
+     */
+    public Page<Label> findPage(Map<String, String> map, int page, int size) {
+        Pageable pageable = PageRequest.of(page-1,size);
+        return labelDao.findAll(getSpecification(map),pageable);
+    }
+
+    /**
+     * 公共的条件方法抽取
+     * @return
+     */
+    public Specification getSpecification(Map<String, String> map){
+       return new Specification<Label>() {
+            /**
+             * @param root 操作根对象
+             * @param criteriaQuery 了解 顶级查询对象
+             * @param criteriaBuilder 条件构建对象
+             * @return
+             */
+            @Nullable
+            @Override
+            public Predicate toPredicate(Root<Label> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
+                // "labelname": "string",  "state": "string",
+
+                String labelname = map.get("labelname");//标签名称  模糊查询
+                String state = map.get("state");//精确匹配
+                //定一个list存放条件
+                List<Predicate> list = new ArrayList<>();
+                if(!StringUtils.isEmpty(labelname)){
+                    list.add(criteriaBuilder.like(root.get("labelname").as(String.class),"%"+labelname+"%")); // labelname like '%%'
+
+                }
+                if(!StringUtils.isEmpty(state)){
+                    list.add(criteriaBuilder.equal(root.get("state").as(String.class),state)); // labelname ='xxx'
+                }
+                if(list == null || list.size() == 0){
+                    return null;//没有条件
+                }
+                //将list转predicate数组
+               /* Predicate[] predicates = new Predicate[list.size()];
+                Predicate[] newPredicates = list.toArray(predicates);
+                return criteriaBuilder.and(newPredicates);*/
+                return criteriaBuilder.and(list.toArray(new Predicate[list.size()]));
+            }
+        };
     }
 }
